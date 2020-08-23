0146051c625595ef13036b718157236afe0f9653
yangjeehan <yangjeehan@naver.com>
2020-08-23 19:52:07 +0900

    bug fix

---
+++ src/main/kotlin/com/chat/config/Main.kt
@@ -0,0 +1,12 @@
+package com.chat.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+}
+
